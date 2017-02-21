93e4129f96f3ae0811e49d24a8b53802dd75bfbd
jiwenfei <981160543@qq.com>
2017-02-21 17:35:21 +0800

    修改返回值json序列化失败

--- src/android/BaiduLocation.java
+++ src/android/BaiduLocation.java
@@ -43,85 +43,31 @@ public class BaiduLocation extends CordovaPlugin {
                 json.put("latitude", location.getLatitude());
                 json.put("lontitude", location.getLongitude());
                 json.put("radius", location.getRadius());
-
-                StringBuilder sb = new StringBuilder(256);
-                sb.append("time : ");
-                sb.append(location.getTime());
-                sb.append("\nerror code : ");
-                sb.append(location.getLocType());
-                sb.append("\nlatitude : ");
-                sb.append(location.getLatitude());
-                sb.append("\nlontitude : ");
-                sb.append(location.getLongitude());
-                sb.append("\nradius : ");
-                sb.append(location.getRadius());
+                json.put("addr", location.getAddrStr());
                 if (location.getLocType() == BDLocation.TypeGpsLocation){// GPS定位结果
-                    sb.append("\nspeed : ");
-                    sb.append(location.getSpeed());// 单位：公里每小时
-                    sb.append("\nsatellite : ");
-                    sb.append(location.getSatelliteNumber());
-                    sb.append("\nheight : ");
-                    sb.append(location.getAltitude());// 单位：米
-                    sb.append("\ndirection : ");
-                    sb.append(location.getDirection());// 单位度
-                    sb.append("\naddr : ");
-                    sb.append(location.getAddrStr());
-                    sb.append("\ndescribe : ");
-                    sb.append("gps定位成功");
-
                     json.put("speed", location.getSpeed());
                     json.put("satellite", location.getSatelliteNumber());
                     json.put("height", location.getAltitude());
                     json.put("direction", location.getDirection());
-                    json.put("addr", location.getAddrStr());
                     json.put("describe", "gps定位成功");
                 } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){// 网络定位结果
-                    sb.append("\naddr : ");
-                    sb.append(location.getAddrStr());
                     //运营商信息
-                    sb.append("\noperationers : ");
-                    sb.append(location.getOperators());
-                    sb.append("\ndescribe : ");
-                    sb.append("网络定位成功");
-
-                    json.put("addr", location.getAddrStr());
                     json.put("operationers", location.getOperators());
                     json.put("describe", "网络定位成功");
                 } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
-                    sb.append("\ndescribe : ");
-                    sb.append("离线定位成功，离线定位结果也是有效的");
-
                     json.put("describe", "离线定位成功，离线定位结果也是有效的");
                 } else if (location.getLocType() == BDLocation.TypeServerError) {
-                    sb.append("\ndescribe : ");
-                    sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-
                     json.put("describe", "服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
                 } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-                    sb.append("\ndescribe : ");
-                    sb.append("网络不同导致定位失败，请检查网络是否通畅");
-
                     json.put("describe", "网络不同导致定位失败，请检查网络是否通畅");
                 } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-                    sb.append("\ndescribe : ");
-
-                    sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
                     json.put("describe", "无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
                 }
-    //            sb.append("\nlocationdescribe : ");
-    //            sb.append(location.getLocationDescribe());// 位置语义化信息
-    //            List<Poi> list = location.getPoiList();// POI数据
-    //            if (list != null) {
-    //                sb.append("\npoilist size = : ");
-    //                sb.append(list.size());
-    //                for (Poi p : list) {
-    //                    sb.append("\npoi= : ");
-    //                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
-    //                }
-    //            }
-                LOG.i(LOG_TAG, sb.toString());
+                //json.put("locationdescribe",location.getLocationDescribe());
+                LOG.e(LOG_TAG, json.toString());
 
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, sb.toString());
+                //PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, sb.toString());
+                PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, json.toString());
                 pluginResult.setKeepCallback(true);
                 cbCtx.sendPluginResult(pluginResult);
             } catch (JSONException e) {
